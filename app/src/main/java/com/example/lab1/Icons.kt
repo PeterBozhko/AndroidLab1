3b16a909f5a0475bd4a2c155f6e3b2c70ad589e3
PeterBozhko <bozhko999@mail.ru>
2022-11-10 13:58:31 +0300

    add Icons

--- app/src/main/java/com/example/lab1/Icons.kt
+++ app/src/main/java/com/example/lab1/Icons.kt
@@ -1,11 +1,11 @@
 package com.example.lab1
 
-enum class Icons(res: Int) {
-    triCopter(R.drawable.tri_copter),
-    quadCopter(R.drawable.tri_copter),
-    y4Copter(R.drawable.tri_copter),
-    hexaCopter(R.drawable.tri_copter),
-    y6Copter(R.drawable.tri_copter),
-    octaCopter(R.drawable.tri_copter),
-    x8Copter(R.drawable.tri_copter)
+enum class Icons(val res: Int) {
+    TriCopter(R.drawable.tri_copter),
+    QuadCopter(R.drawable.quad_copter),
+    Y4Copter(R.drawable.y4_copter),
+    HexaCopter(R.drawable.hexa_copter),
+    Y6Copter(R.drawable.y6_copter),
+    OctaCopter(R.drawable.octo_copter),
+    X8Copter(R.drawable.x8_copter)
 }
