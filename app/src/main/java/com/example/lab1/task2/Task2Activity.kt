7594e77eaf7a218c5e87930ff2e77152982cfb91
p.bozhko <p.bozhko@geoscan.aero>
2022-12-06 15:56:15 +0300

    add threads, threadlock, speed btns

--- app/src/main/java/com/example/lab1/task2/Task2Activity.kt
+++ app/src/main/java/com/example/lab1/task2/Task2Activity.kt
@@ -6,23 +6,28 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.lab1.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Task2Activity: AppCompatActivity() {
     private lateinit var scoreTV1: TextView
     private lateinit var scoreTV2: TextView
-    private var lock = false
-
+    private lateinit var viewModel: Task2ViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task2)
-        val viewModel: Task2ViewModel =  ViewModelProvider(this).get(Task2ViewModel::class.java)
         scoreTV1 = findViewById(R.id.textView1)
         scoreTV2 = findViewById(R.id.textView2)
+        viewModel = ViewModelProvider(this)[Task2ViewModel::class.java]
         val runBtn = findViewById<Button>(R.id.buttonRun)
         val stopBtn = findViewById<Button>(R.id.buttonStop)
         val resetBtn = findViewById<Button>(R.id.buttonReset)
+        val speedUpBtn1 = findViewById<FloatingActionButton>(R.id.speed_up_btn1)
+        val speedDownBtn1 = findViewById<FloatingActionButton>(R.id.speed_down_btn1)
+        val speedUpBtn2 = findViewById<FloatingActionButton>(R.id.speed_up_btn2)
+        val speedDownBtn2 = findViewById<FloatingActionButton>(R.id.speed_down_btn2)
 
         viewModel.score1.observe(this) { scoreTV1.text = it.toString() }
+        viewModel.score2.observe(this) { scoreTV2.text = it.toString() }
         runBtn.setOnClickListener {
             viewModel.onRun()
         }
@@ -30,5 +35,23 @@ class Task2Activity: AppCompatActivity() {
         stopBtn.setOnClickListener {
             viewModel.onStop()
         }
+
+        resetBtn.setOnClickListener{
+            viewModel.onReset()
+        }
+        speedUpBtn1.setOnClickListener{ viewModel.speedUpThread1()}
+        speedDownBtn1.setOnClickListener{ viewModel.speedDownThread1()}
+        speedUpBtn2.setOnClickListener{ viewModel.speedUpThread2()}
+        speedDownBtn2.setOnClickListener{ viewModel.speedDownThread2()}
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onRun()
     }
 }
--- app/src/main/java/com/example/lab1/task2/Task2ViewModel.kt
+++ app/src/main/java/com/example/lab1/task2/Task2ViewModel.kt
@@ -3,28 +3,88 @@ package com.example.lab1.task2
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.*
+import java.lang.Thread.sleep
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.ReentrantLock
 
 class Task2ViewModel: ViewModel() {
     val score1 = MutableLiveData<Int>(0)
     val score2 = MutableLiveData<Int>(0)
-    var runJob: Job = Job()
+    private val isLock : AtomicBoolean = AtomicBoolean()
+    @Volatile private var thread1Frequency = 1.5f
+    @Volatile private var thread2Frequency = 1f
+    private val locker: ReentrantLock = ReentrantLock()
+    private val condition: Condition = locker.newCondition()
     private val tag = "Task2ViewModel"
     init {
         score1.value = 0
         score2.value = 0
     }
     fun onRun(){
-        runJob = CoroutineScope(Dispatchers.Main).launch {
-            while (true){
-                score1.value = score1.value?.inc()
-                Log.d(tag, "${score1.value}")
-                delay(800)
-            }
+        isLock.set(false)
+        if (!thread1.isAlive && !thread2.isAlive){
+            thread1.start()
+            thread2.start()
+            return
         }
+        locker.lock()
+        condition.signalAll()
+        locker.unlock()
     }
     fun onStop(){
-        runJob.cancel()
+        isLock.set(true)
+    }
+    fun onReset(){
+        score1.value = 0
+        score2.value = 0
     }
 
+    private val thread1 = Thread{
+        while (true){
+            while (isLock.get()) {
+                locker.lock()
+                try {
+                    condition.await()
+                } catch (e: InterruptedException) {
+                    Log.e(tag, "$e")
+                    return@Thread
+                } finally {
+                    locker.unlock()
+                }
+            }
+            score1.postValue(score1.value?.inc())
+            sleep((1000 / thread1Frequency).toLong())
+        }
+    }
+    private val thread2 = Thread{
+        while (true) {
+            while (isLock.get()) {
+                locker.lock()
+                try {
+                    condition.await()
+                } catch (e: InterruptedException) {
+                    Log.e(tag, "$e")
+                    return@Thread
+                } finally {
+                    locker.unlock()
+                }
+            }
+            score2.postValue(score2.value?.inc())
+            sleep((1000 / thread2Frequency).toLong())
+        }
+    }
+
+    fun speedUpThread1(){
+        if (thread1Frequency < 1000) thread1Frequency += 0.5f
+    }
+    fun speedUpThread2(){
+        if (thread2Frequency < 1000) thread2Frequency += 0.5f
+    }
+    fun speedDownThread1(){
+        if (thread1Frequency > 0.5f) thread1Frequency -= 0.5f
+    }
+    fun speedDownThread2(){
+        if (thread2Frequency > 0.5f) thread2Frequency -= 0.5f
+    }
 }
