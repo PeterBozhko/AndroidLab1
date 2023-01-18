887d1246dbe37524b708438ff1a600a65dc75a04
p.bozhko <p.bozhko@geoscan.aero>
2023-01-18 13:11:26 +0300

    update Interactor

--- app/src/main/java/com/example/lab1/task3/domain/Interactor.kt
+++ app/src/main/java/com/example/lab1/task3/domain/Interactor.kt
@@ -2,9 +2,7 @@ package com.example.lab1.task3.domain
 
 import androidx.lifecycle.MutableLiveData
 import com.example.lab1.RetrofitService
-import com.example.lab1.task3.models.Book
-import com.example.lab1.task3.models.BookRequest
-import com.example.lab1.task3.models.Event
+import com.example.lab1.task3.models.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,24 +10,23 @@ import kotlinx.coroutines.launch
 object Interactor {
     private val api = RetrofitService.client
 
-    fun createBook(book: BookRequest, event: MutableLiveData<Event>){
+    fun createBook(book: BookRequest, event: MutableLiveData<Event>): Int{
         val booksResponse = api.addBook(book)
         event.postValue(Event.loading())
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val result = booksResponse.execute()
-                if (result.isSuccessful){
-                    if (result.body() != null){
-                        event.postValue(Event.success("Успешно загружено"))
-                    }else{
-                        event.postValue(Event.success("Запрос не обработан"))
-                    }
-                } else event.postValue(Event.error(result.message(), result.code()))
-            }catch (e: Exception){
-                event.postValue(Event.error(e.message,null))
-            }
+        try {
+            val result = booksResponse.execute()
+            if (result.isSuccessful){
+                if (result.body() != null){
+                    event.postValue(Event.success("Успешно загруженo"))
+                    return Integer.valueOf(result.body()!!.message.substring(32))
+                }else{
+                    event.postValue(Event.success("Запрос не обработан"))
+                }
+            } else event.postValue(Event.error(result.message(), result.code()))
+        }catch (e: Exception){
+            event.postValue(Event.error(e.message,null))
         }
-
+        return 0
     }
 
     fun downloadBooks(event: MutableLiveData<Event>): MutableList<Book>?{
@@ -89,20 +86,117 @@ object Interactor {
         }
     }
 
-    fun updateBook(id: Int, book: Book, event: MutableLiveData<Event>){
+    fun updateBook(id: Int, book: BookRequest, event: MutableLiveData<Event>){
         val booksResponse = api.updateBook(id, book)
         event.postValue(Event.loading())
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = booksResponse.execute()
+                if (result.isSuccessful) {
+                    if (result.body() != null) {
+                        event.postValue(Event.success("Успешно загружено"))
+                    } else {
+                        event.postValue(Event.success("Запрос не обработан"))
+                    }
+                } else event.postValue(Event.error(result.message(), result.code()))
+            } catch (e: Exception) {
+                event.postValue(Event.error(e.message, null))
+            }
+        }
+    }
+
+    fun createAuthor(author: AuthorRequest, event: MutableLiveData<Event>): Int{
+        val authorResponse = api.addAuthor(author)
+        event.postValue(Event.loading())
         try {
-            val result = booksResponse.execute()
+            val result = authorResponse.execute()
             if (result.isSuccessful){
                 if (result.body() != null){
                     event.postValue(Event.success("Успешно загружено"))
+                    return Integer.valueOf(result.body()!!.message.substring(34))
                 }else{
                     event.postValue(Event.success("Запрос не обработан"))
                 }
             } else event.postValue(Event.error(result.message(), result.code()))
         }catch (e: Exception){
             event.postValue(Event.error(e.message,null))
+        }
+        return 0
+    }
+
+    fun downloadAuthors(event: MutableLiveData<Event>): MutableList<Author>?{
+        val authorResponse = api.getAuthors()
+        event.postValue(Event.loading())
+        try {
+            val result = authorResponse.execute()
+            if (result.isSuccessful){
+                if (result.body() != null){
+                    event.postValue(Event.success("Успешно загружено"))
+                    return result.body()!!.toMutableList()
+                }else{
+                    event.postValue(Event.success("Авторы не найдены"))
+                }
+            } else event.postValue(Event.error(result.message(), result.code()))
+        }catch (e: Exception){
+            event.postValue(Event.error(e.message,null))
+        }
+        return null
+    }
+
+    fun geAuthorById(id: Int, event: MutableLiveData<Event>): Author?{
+        val authorResponse = api.getAuthor(id)
+        event.postValue(Event.loading())
+        try {
+            val result = authorResponse.execute()
+            if (result.isSuccessful){
+                if (result.body() != null){
+                    event.postValue(Event.success("Успешно загружено"))
+                    return result.body()
+                }else{
+                    event.postValue(Event.success("Запрос не обработан"))
+                }
+            } else event.postValue(Event.error(result.message(), result.code()))
+        }catch (e: Exception){
+            event.postValue(Event.error(e.message,null))
+        }
+        return null
+    }
+
+    fun deleteAuthorByID(id: Int, event: MutableLiveData<Event>){
+        val authorResponse = api.deleteAuthor(id)
+        event.postValue(Event.loading())
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = authorResponse.execute()
+                if (result.isSuccessful) {
+                    if (result.body() != null) {
+                        event.postValue(Event.success("Успешно загружено"))
+                    } else {
+                        event.postValue(Event.success("Запрос не обработан"))
+                    }
+                } else event.postValue(Event.error(result.message(), result.code()))
+            } catch (e: Exception) {
+                event.postValue(Event.error(e.message, null))
+            }
+        }
+    }
+
+    fun updateAuthor(id: Int, author: AuthorRequest, event: MutableLiveData<Event>){
+        val authorResponse = api.updateAuthor(id, author)
+        event.postValue(Event.loading())
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = authorResponse.execute()
+                if (result.isSuccessful) {
+                    if (result.body() != null) {
+                        event.postValue(Event.success("Успешно загружено"))
+                    } else {
+                        event.postValue(Event.success("Запрос не обработан"))
+                    }
+                } else event.postValue(Event.error(result.message(), result.code()))
+            } catch (e: Exception) {
+                event.postValue(Event.error(e.message, null))
+            }
         }
     }
 }
