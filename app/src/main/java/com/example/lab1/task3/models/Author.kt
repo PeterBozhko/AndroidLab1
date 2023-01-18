1368e23a8b236eacccdcaee188b023c3cb4a19c1
p.bozhko <p.bozhko@geoscan.aero>
2023-01-18 13:17:21 +0300

    update Author Model

--- app/src/main/java/com/example/lab1/task3/models/Author.kt
+++ app/src/main/java/com/example/lab1/task3/models/Author.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Author(val id: Int, val firstName: String, val lastName: String, val year: Int) :
+data class Author(val id: Int, var firstName: String, var lastName: String, var year: Int) :
     Parcelable {
     override fun toString(): String {
         return "($id) $firstName $lastName, $year"
